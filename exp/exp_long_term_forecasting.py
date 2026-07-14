d8f29f99af82581c27ff09a105ae9c8678afb89f
Steve D <sdonoghue1@sheffield.ac.uk>
2026-07-14 11:58:27 +0100

    Include date and time in results output
    Added timestamp to results file for long-term forecasting.

--- exp/exp_long_term_forecasting.py
+++ exp/exp_long_term_forecasting.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 from torch import optim
 import os
 import time
+from datetime import datetime
 import warnings
 import numpy as np
 from utils.dtw_metric import dtw, accelerated_dtw
@@ -255,6 +256,8 @@ class Exp_Long_Term_Forecast(Exp_Basic):
         mae, mse, rmse, mape, mspe = metric(preds, trues)
         print('mse:{}, mae:{}, dtw:{}'.format(mse, mae, dtw))
         f = open("result_long_term_forecast.txt", 'a')
+        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+        f.write("date and time:",date_time)
         f.write(setting + "  \n")
         f.write('mse:{}, mae:{}, dtw:{}'.format(mse, mae, dtw))
         f.write('\n')
