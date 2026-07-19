2e99edd13253501528f07635dee7e84b7f663363
Steve D <sdonoghue1@sheffield.ac.uk>
2026-07-19 13:26:04 +0100

    Implement save_args function to log configuration
    Added a function to save arguments to a text file with formatted output.

--- utils/print_args.py
+++ utils/print_args.py
@@ -1,3 +1,4 @@
+import os
 def print_args(args):
     print("\033[1m" + "Basic Config" + "\033[0m")
     print(f'  {"Task Name:":<20}{args.task_name:<20}{"Is Training:":<20}{args.is_training:<20}')
@@ -56,3 +57,54 @@ def print_args(args):
     p_hidden_dims_str = ', '.join(map(str, args.p_hidden_dims))
     print(f'  {"P Hidden Dims:":<20}{p_hidden_dims_str:<20}{"P Hidden Layers:":<20}{args.p_hidden_layers:<20}') 
     print()
+def save_args(args):
+    f = open("result_long_term_forecast_setting.txt", 'a')
+    f.write("\033[1m" + "Basic Config" + "\033[0m\n")
+    f.write(f'  {"Task Name:":<20}{args.task_name:<20}{"Is Training:":<20}{args.is_training:<20}\n')
+    f.write(f'  {"Model ID:":<20}{args.model_id:<20}{"Model:":<20}{args.model:<20}\n')
+    f.write('\n')
+
+    f.write("\033[1m" + "Data Loader" + "\033[0m\n")
+    f.write(f'  {"Data:":<20}{args.data:<20}{"Root Path:":<20}{args.root_path:<20}\n')
+    f.write(f'  {"Data Path:":<20}{args.data_path:<20}{"Features:":<20}{args.features:<20}\n')
+    f.write(f'  {"Target:":<20}{args.target:<20}{"Freq:":<20}{args.freq:<20}\n')
+    f.write(f'  {"Checkpoints:":<20}{args.checkpoints:<20}\n')
+    f.write('\n')
+
+    if args.task_name in ['long_term_forecast', 'short_term_forecast']:
+        f.write("\033[1m" + "Forecasting Task" + "\033[0m"\n)
+        f.write(f'  {"Seq Len:":<20}{args.seq_len:<20}{"Label Len:":<20}{args.label_len:<20}'\n)
+        f.write(f'  {"Pred Len:":<20}{args.pred_len:<20}{"Seasonal Patterns:":<20}{args.seasonal_patterns:<20}\n')
+        f.write(f'  {"Inverse:":<20}{args.inverse:<20}\n')
+        f.write('\n')
+
+    f.write("\033[1m" + "Model Parameters" + "\033[0m\n")
+    f.write(f'  {"Top k:":<20}{args.top_k:<20}{"Num Kernels:":<20}{args.num_kernels:<20}\n')
+    f.write(f'  {"Enc In:":<20}{args.enc_in:<20}{"Dec In:":<20}{args.dec_in:<20}\n')
+    f.write(f'  {"C Out:":<20}{args.c_out:<20}{"d model:":<20}{args.d_model:<20}\n')
+    f.write(f'  {"n heads:":<20}{args.n_heads:<20}{"e layers:":<20}{args.e_layers:<20}\n')
+    f.write(f'  {"d layers:":<20}{args.d_layers:<20}{"d FF:":<20}{args.d_ff:<20}\n')
+    f.write(f'  {"Moving Avg:":<20}{args.moving_avg:<20}{"Factor:":<20}{args.factor:<20}\n')
+    f.write(f'  {"Distil:":<20}{args.distil:<20}{"Dropout:":<20}{args.dropout:<20}\n')
+    f.write(f'  {"Embed:":<20}{args.embed:<20}{"Activation:":<20}{args.activation:<20}\n')
+    f.write('\n')
+
+    f.write("\033[1m" + "Run Parameters" + "\033[0m\n")
+    f.write(f'  {"Num Workers:":<20}{args.num_workers:<20}{"Itr:":<20}{args.itr:<20}\n')
+    f.write(f'  {"Train Epochs:":<20}{args.train_epochs:<20}{"Batch Size:":<20}{args.batch_size:<20}\n')
+    f.write(f'  {"Patience:":<20}{args.patience:<20}{"Learning Rate:":<20}{args.learning_rate:<20}'\n)
+    f.write(f'  {"Des:":<20}{args.des:<20}{"Loss:":<20}{args.loss:<20}\n')
+    f.write(f'  {"Lradj:":<20}{args.lradj:<20}{"Use Amp:":<20}{args.use_amp:<20}\n')
+    f.write('\n')
+
+    f.write("\033[1m" + "GPU" + "\033[0m\n")
+    f.write(f'  {"Use GPU:":<20}{args.use_gpu:<20}{"GPU:":<20}{args.gpu:<20\n}')
+    f.write(f'  {"Use Multi GPU:":<20}{args.use_multi_gpu:<20}{"Devices:":<20}{args.devices:<20}\n')
+    f.write()
+
+    f.write("\033[1m" + "De-stationary Projector Params" + "\033[0m\n")
+    p_hidden_dims_str = ', '.join(map(str, args.p_hidden_dims))
+    f.write(f'  {"P Hidden Dims:":<20}{p_hidden_dims_str:<20}{"P Hidden Layers:":<20}{args.p_hidden_layers:<20}\n') 
+    f.write('\n')
+    f.close()
+
