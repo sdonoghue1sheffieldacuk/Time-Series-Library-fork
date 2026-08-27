38d8907da3ed693b6bca400b6dc474637f6f788c
sdonoghue1sheffieldacuk <sdonoghue1@sheffield.ac.uk>
2026-08-27 13:36:39 +0100

    implement test only loader

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -90,3 +90,35 @@ def data_provider(args, flag):
             num_workers=args.num_workers,
             drop_last=drop_last)
         return data_set, data_loader
+    
+def data_provider_test_only(args, flag):
+    Data = data_dict[args.data]
+    timeenc = 0 if args.embed != 'timeF' else 1
+
+    shuffle_flag = False if (flag == 'test' or flag == 'TEST') else True
+    drop_last = False
+    batch_size = args.batch_size
+    freq = args.freq
+
+    data_set = Data(
+        args = args,
+        root_path=args.root_path,
+        data_path=args.data_path,
+        flag=flag,
+        size=[args.seq_len, args.label_len, args.pred_len],
+        features=args.features,
+        target=args.target,
+        timeenc=timeenc,
+        freq=freq,
+        seasonal_patterns=args.seasonal_patterns
+    )
+    print(flag, len(data_set))
+    print(os.path.join(args.root_path, args.data_path))
+    print(os.getcwd())
+    data_loader = DataLoader(
+        data_set,
+        batch_size=batch_size,
+        shuffle=shuffle_flag,
+        num_workers=args.num_workers,
+        drop_last=drop_last)
+    return data_set, data_loader
